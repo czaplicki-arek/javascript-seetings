dfe6f928efa993142fce35fd22c12f6c8e78e578
czaplicki-arek <40891534+czaplicki-arek@users.noreply.github.com>
2018-07-27 10:09:28 +0200

    Update JavaScript_02IE.kt

--- .teamcity/JavaScript/buildTypes/JavaScript_02IE.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_02IE.kt
@@ -1,1 +1,19 @@
 
+package JavaScript.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2018_1.*
+
+object JavaScript_02IE : BuildType({
+    templates(JavaScript_Template)
+    uuid = "javascript_02ie"
+    name = "02. Internet Explorer"
+
+    params {
+        param("Browser", "IE")
+    }
+
+    dependencies {
+        snapshot(JavaScript_01FastTest) {
+        }
+    }
+})
